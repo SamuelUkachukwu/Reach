39d9ae527942d5a5d136c5fdc5ccc171e9826a42
SamuelUkachukwu <shybold@yahoo.com>
2023-02-06 01:04:06 +0000

    Added search functionality.

--- giftappy/settings.py
+++ giftappy/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
         },
     },
 ]
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
--- home/views.py
+++ home/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from wishes.models import Profile
+
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def home(request):
     template_name = 'home/index.html'
-    context = {}
+    items = User.objects.all()
+    context = {
+        'items': items,
+    }
     return render(request, template_name, context)
+
+
+def search(request):
+    query = None
+    items = User.objects.all()
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            return redirect(reverse('home'))
+
+        queries = Q(name__icontains=query)
+        items = users.filter(queries)
+    return render(request, 'home/search.html', {'items': items})
